ea253c460f1154a7f5d5ef44ed47a8a613b9e19b
Atilla <atillacolgecen@gmail.com>
2024-06-10 01:24:10 +0200

    New changes

--- init-dev-tools.sh
+++ init-dev-tools.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 
-# Update the system package index
-echo "Updating system package index"
+# Update package list
+echo "Updating package list..."
 sudo apt update
 
-# Install necessary dependencies
-echo "Installing software properties"
-sudo apt install -y software-properties-common apt-transport-https wget
+# Fix broken packages
+echo "Fixing broken packages..."
+sudo apt --fix-broken install -y
 
-# Add the Ansible PPA (Personal Package Archive)
-echo "Adding Ansible repository"
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-
-# Install Ansible
-echo "Installing Ansible"
-sudo apt install -y ansible
-
-# Add the Git PPA and install Git
-echo "Installing Git"
+# Install Git
+echo "Installing Git..."
 sudo apt install -y git
 
-# Add the Microsoft GPG key and VS Code repository
-echo "Adding Microsoft GPG key and VS Code repository"
+# Install KeePassXC
+echo "Installing KeePassXC..."
+sudo apt install -y keepassxc
+
+# Install Visual Studio Code
+echo "Installing Visual Studio Code..."
+# Import the Microsoft GPG key
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 
-# Update package index again after adding new repository
-echo "Updating package index after adding VS Code repository"
+# Update package list after adding VSCode repo
 sudo apt update
-
-# Install Visual Studio Code
-echo "Installing Visual Studio Code"
 sudo apt install -y code
 
-# Upgrade the system to the latest versions of all packages
-echo "Upgrading all system packages"
-sudo apt update -y
+# Install Ansible
+echo "Installing Ansible..."
+sudo apt install -y ansible
+
+# Upgrade all packages to the latest version
+echo "Upgrading all packages..."
 sudo apt upgrade -y
 
-# Optional: Clean up unused packages and dependencies
-echo "Cleaning up unused packages"
+# Clean up unused packages
+echo "Cleaning up unused packages..."
 sudo apt autoremove -y
+
+# Clean up apt cache
+echo "Cleaning up apt cache..."
 sudo apt clean
 
-echo "Installation of Ansible, Git, and VS Code and system upgrade complete"
+echo "All specified applications have been installed and the system is rebooting."
 
+# Reboot the system
+echo "Rebooting the system..."
+sudo reboot
