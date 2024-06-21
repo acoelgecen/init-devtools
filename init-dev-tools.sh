367048c0e1d9512f6f98d56b23b950f5c6840b45
Atilla <atilla.coelgecen@optimizemyday.com>
2024-06-21 13:15:33 +0200

    Script changes

--- init-dev-tools.sh
+++ init-dev-tools.sh
@@ -3,108 +3,87 @@
 GREEN='\033[0;32m'  # ANSI color code for green
 YELLOW='\033[0;33m' # ANSI color code for yellow
 RED='\033[0;31m'    # ANSI color code for red
-NC='\033[0m'        # No color / reset ANSI code
+NOCOLOR='\033[0m'   # No color / reset ANSI code
 
-# Function to check the success of the last command
+# FuNOCOLORtion to check the success of the last command
 check_command() {
     if [ $? -eq 0 ]; then
-        echo -e "${GREEN}Command executed successfully.${NC}"
+        echo -e "${GREEN}Command executed successfully.${NOCOLOR}"
     else
-        echo -e "${RED}Command failed.${NC}"
+        echo -e "${RED}Command failed.${NOCOLOR}"
         exit 1  # Exit the script if any command fails
     fi
 }
 
+
 # Ask for sudo upfront
-echo -e "${GREEN}This script requires sudo privileges. Please enter your password.${NC}"
+echo -e "${YELLOW}This script requires sudo privileges. Please enter your password.${NOCOLOR}"
 sudo -v
 check_command
 echo ""
 
+
 # Update package list
-echo -e "${GREEN}Updating package list...${NC}"
+echo -e "${YELLOW}Updating package list...${NOCOLOR}"
 sudo apt update -y
 check_command
 sudo apt upgrade -y
 check_command
 echo ""
 
-sleep 1
 
 # Install Curl
-echo -e "${GREEN}Installing Curl...${NC}"
+echo -e "${YELLOW}Installing Curl...${NOCOLOR}"
 sudo apt install -y curl
 check_command
 echo ""
 
-sleep 1
 
 # Install Git
-echo -e "${GREEN}Installing Git...${NC}"
+echo -e "${YELLOW}Installing Git...${NOCOLOR}"
 sudo apt install -y git
 check_command
 echo ""
 
-sleep 1
 
 # Configure Git
 echo "Configuring Git..."
 git config --global user.name "Atilla"
 git config --global user.email "atilla.coelgecen@optimizemyday.com"
 check_command
+echo ""
 
-sleep 1
 
 # Install build-essential
-echo -e "${GREEN}Installing build-essential...${NC}"
+echo -e "${YELLOW}Installing build-essential...${NOCOLOR}"
 sudo apt install -y build-essential
 check_command
 echo ""
 
-sleep 1
 
 # Install KeePassXC
-echo -e "${GREEN}Installing KeePassXC...${NC}"
+echo -e "${YELLOW}Installing KeePassXC...${NOCOLOR}"
 sudo apt install -y keepassxc
 check_command
 echo ""
 
-sleep 1
 
 # Install Ansible
-echo -e "${GREEN}Installing Ansible...${NC}"
+echo -e "${YELLOW}Installing Ansible...${NOCOLOR}"
 sudo apt install -y ansible
 check_command
 echo ""
 
-sleep 1
 
 # Upgrade all packages to the latest version
-echo -e "${GREEN}Upgrading all packages...${NC}"
+echo -e "${YELLOW}Upgrading all packages...${NOCOLOR}"
 sudo apt upgrade -y
 check_command
 echo ""
 
-sleep 1
-
-# Clean up unused packages
-echo -e "${GREEN}Cleaning up unused packages...${NC}"
-sudo apt autoremove -y
-check_command
-echo ""
-
-sleep 1
-
-# Clean up apt cache
-echo -e "${GREEN}Cleaning up apt cache...${NC}"
-sudo apt clean
-check_command
-echo ""
-
-sleep 1
 
 # Install Visual Studio Code repository and application
-echo -e "${GREEN}Installing Visual Studio Code...${NC}"
+echo -e "${YELLOW}Installing Visual Studio Code...${NOCOLOR}"
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
@@ -116,14 +95,38 @@ sudo apt install -y code
 check_command
 echo ""
 
-sleep 1
 
-echo -e "${GREEN}All specified applications have been installed.${NC}"
+# Preparing SSH folder and files
+echo -e "${YELLOW}Preparing SSH folder and files${NOCOLOR}"
+sudo mkdir .ssh
+touch ~/.ssh/id_rsa
+touch ~/.ssh/id_rsa.pub
+touch ~/.ssh/config
+
+chmod 600 ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa.pub
+
+
+# Clean up unused packages
+echo -e "${YELLOW}Cleaning up unused packages...${NOCOLOR}"
+sudo apt autoremove -y
+check_command
 echo ""
 
-sleep 1
+
+# Clean up apt cache
+echo -e "${YELLOW}Cleaning up apt cache...${NOCOLOR}"
+sudo apt clean
+check_command
+echo ""
+
+
+# Finish message
+echo -e "${YELLOW}All specified applications have been installed.${NOCOLOR}"
+echo ""
+
 
 # Prompt to reboot
-read -p "Press any key to reboot your system now, or Ctrl+C to cancel..." -n1 -s
+read -p "Press any key to reboot your system now, or Ctrl+C to caNOCOLORel..." -n1 -s
 echo -e "\nRebooting the system..."
 sudo reboot
